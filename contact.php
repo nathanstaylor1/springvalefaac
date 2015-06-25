026019d225f19cb48058333eb70e911268646990
NathanMBP <nathanstaylor1@gmail.com>
2015-06-25 12:43:47 +1000

    first draft

---
+++ contact.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html" charset="utf-8" />   
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" href="http://mplus-fonts.sourceforge.jp/webfonts/mplus_webfonts.css" />  
+	<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<link rel='stylesheet' type="text/css" href='resources/css/style.css'/>
+
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+    <script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.9.1/jquery-ui.min.js"></script>
+	<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+	<script type='text/javascript' src='resources/js/script.js'></script>
+</head>
+<body>
+
+  <?php include 'navbar.php';?>
+
+
+
+
+  <?php include 'footer.php';?>
+
+</body>
+
+</html>
---
+++ navbar.php
@@ -0,0 +1,24 @@
+	 <nav class="navbar navbar-default">
+	  <div class="container">
+	    <div class="navbar-header">
+	      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	        <span class="icon-bar"></span>
+	      </button>
+	      <a class="navbar-brand" href="index.php">
+	      	<img id = "logo" alt = "logo" src = "resources/css/images/logo_and_text_alpha1.png" class = "img-responsive hidden-xs hidden-sm">
+	      	<img id = "logo-smaller" alt = "logo" src = "resources/css/images/logo_and_text_alpha1.png" class = "img-responsive hidden-lg hidden-md"></a>
+	    </div>
+	    <div class="collapse navbar-collapse" id="myNavbar">
+	      <ul class="nav navbar-nav navbar-right">
+	        <li><a href="index.php">Home</a></li>
+	        <li><a href="about.php">About</a></li>
+	        <li><a href="about.php">Other1</a></li>
+	        <li><a href="about.php">Other2</a></li>
+	        <li><a href="about.php">Other3</a></li>
+	        <li><a href="contact.php">Contact</a></li>
+	      </ul>
+	    </div>
+	  </div>
+	</nav>
