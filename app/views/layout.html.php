06372227b91eb516e5a78357974d3f0371a7dbc8
NathanMBP <nathanstaylor1@gmail.com>
2015-07-14 14:29:52 +1000

    update links to secure connections

--- app/views/layout.html.php
+++ app/views/layout.html.php
@@ -8,16 +8,16 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" user-scalable="no" />
 
 	<link rel="alternate" type="application/rss+xml" title="<?php echo config('blog.title')?>  Feed" href="<?php echo site_url()?>rss" />
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,600' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,600' rel='stylesheet' type='text/css'>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 	<link rel="stylesheet/less" type="text/css" href="<?php echo site_url() ?>assets/css/style.less" />
 
 	<link rel="shortcut icon" href="<?php echo site_url() ?>assets/media/favicon/favicon.ico" type="image/x-icon"/>
 
 	<script src="<?php echo site_url() ?>assets/js/less.min.js" type="text/javascript"></script>
-	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+	<script src="https://code.jquery.com/jquery-latest.min.js"></script>
 	<script src="https://maps.googleapis.com/maps/api/js"></script>
-    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <script type='text/javascript' src="<?php echo site_url() ?>assets/js/script.js"></script>
 	
     <!--[if lt IE 9]>
